c4cf4ae75128da0aa76b08b20d3a932d92bd0629
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2017-10-27 13:58:19 -0700

    ok

--- ShoppingSpree/ShoppingSpree1.java
+++ ShoppingSpree/ShoppingSpree1.java
@@ -1,52 +1,58 @@
-import java. until.*;
+import java.util.*;
 
 public class ShoppingSpree1{
 
   int budget;
+  String name;
   ArrayList<String> bags;
 
-  public ShoppingSpree1(){
-    budget=2000;
+  public ShoppingSpree1(int money){
+    budget= money;
     bags= new ArrayList<String>();
+    name = "Maggie";
 }
 
-  public int getMoney(){
+  public int getMoney() {
     return budget;
 }
 
   public void getBags(){
     System.out.println("You are carrying: ");
     for(int i=0; i< bags.size(); i++){
-      System.out. print(bags.get(i)+", ");
-  }
-    Sydtem.out.println(" and that's it!");
+      System.out.print(bags.get(i)+", ");
+    }
+    System.out.println("and that's it!");
 }
 
   public void buyShoes() {
     System.out.println("Converse or Yeezy?");
     Scanner input2= new Scanner(System.in);
-    if (input2.equals("Converse")){
-      bags.add("Shoes");
+    String brand = input2.nextLine();
+
+    if (brand.equals("Converse")){
+      bags.add("Converse");
       budget = budget- 60;
     }
-    else if (input2.equals("Yeezy")){
-      bags.add("Shoes");
+    else if (brand.equals("Yeezy")){
+      bags.add("Yeezy");
       budget = budget - 85;
     }
     else {
       System.out.println("NOT a valid option. Please double check your spelling.");
     }
-  }}
+  }
 
   public void buyPhone() {
     System.out.println("Nokia or Iphone?");
     Scanner input3= new Scanner(System.in);
-    if (input3.equals("Nokia")){
-      bags.add("Phone");
+    String brand2 = input3.nextLine();
+
+    if (brand2.equals("Nokia")){
+      bags.add("Nokia");
       budget = budget- 10;
     }
-    else if (input3.equals("Iphone")){
-      bags.add("Phone");
+    else if (brand2.equals("Iphone")){
+      bags.add("Iphone");
       budget = budget - 800;
     }
     else {
@@ -57,20 +63,29 @@ public class ShoppingSpree1{
   public void buyFood() {
     System.out.println("Fast food or Fancy restaurant?");
     Scanner input4= new Scanner(System.in);
-    if (input4.equals("Fast food")){
-      bags.add("Food");
+    String brand3 = input4.nextLine();
+    if (brand3.equals("Fast food")){
+      bags.add("fast food");
       budget = budget - 20;
     }
-    else if (input4.equals("Fancy restaurant")){
-      bags.add("Food");
-      budget = budget - 100;
+    else if (brand3.equals("Fancy restaurant")){
+      bags.add("fancy restaurant");
+      budget = budget - 250;
     }
     else {
       System.out.println("NOT a valid option. Please double check your spelling.");
     }
   }
 
-ShoppingSpree spree = new ShoppingSpree(money);
+  public static void main(String[] args){
+
+        System.out.println("Welcome to your Shopping Spree!");
+        System.out.println("How much money have you brought today?");
+        Scanner amount = new Scanner(System.in);
+        int money = Integer.parseInt(amount.nextLine());
+
+        ShoppingSpree1 spree = new ShoppingSpree1(money);
+
 
         spree.buyShoes();
         spree.buyPhone();
@@ -79,6 +94,6 @@ ShoppingSpree spree = new ShoppingSpree(money);
         System.out.println("You have " + spree.getMoney() + " dollars left.");
         spree.getBags();
 
-    }
+  }
 
 }
