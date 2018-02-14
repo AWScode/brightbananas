a20b28e15d1f27647f44b3498342b880d4873650
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2018-02-14 11:39:31 -0800

    wow

--- Directory/Student.java
+++ Directory/Student.java
@@ -7,12 +7,61 @@
 public class Student{
 
   public static String name;
-  public static int age;
-  public static double height;
-  public static int gradePercent;
+  public static int grade;
   public static String hobby;
-  public static String favSubject;
+  public static String subject;
+  public static String teacher;
 
-  public Student(String n, int a, double h, int g, String o, String f) {
+  public Student(String n, int g, String o, String f, String t) {
+    this.name = n;
+    this.grade = g;
+    this.hobby = o;
+    this.Subject = f;
+    this.teacher = t
+  }
+
+  public static void hello(){
+    System.out.println("Hi guy!");
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getGrade(){
+    return grade;
+  }
+
+  public String getHobby(){
+    return hobby;
+  }
+
+  public String getSubject(){
+    return subject;
+  }
+
+  public String getTeacher(){
+    return teacher;
+  }
+
+  public void setName(String n){
+    name = n;
+  }
+
+  public void setGrade(int g){
+    grade = g;
+  }
+
+  public void setHobby(String h){
+    hobby = h;
+  }
+
+  public void setSubject(String f){
+    subject = f
+  }
+
+  public void setTeacher(String t){
+    teacher = t
+  }
 
 }
--- Directory/Subject.java
+++ Directory/Subject.java
@@ -6,4 +6,26 @@
 
 public class Subject{
 
-  
+  public static void main(String[] args) {
+   System.out.println("Welcome come to subject check!");
+   System.out.println("Here we provide a subject list!");
+
+   Subject ceramics = new Subject("Maggie", 11, "ice skating", "ceramics", "Mrs. Haha");
+
+   Subject robotics = new Subject("Smart", 9, "hacking", "robotics", "Mr. Joke");
+
+   Subject dance = new Subject("Lily", 12, "play piano", "dance", "Ms. Yan");
+
+   Subject love skills= new Subject("Rose", 8, "horse riding", "love skills", "Mr. Jack");
+
+   Subject read mind = new Subject("Dumbledore", 10, "invent things", "read mind", "Ms. Hermione");
+
+   Subject magic = new Subject("Harry", 11, "Quidditch", "magic", "Mr. Potter");
+
+   System.out.println("Let me proudly introduce:");
+
+   ceramics.hello();
+   String newSubject = ceramics.getSubject();
+   System.out.println("This student is taking: " + newSubject);
+ }
+}
