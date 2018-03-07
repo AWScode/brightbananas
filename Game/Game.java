3ef33091ba2772114e11a2e84c5f6752f0655d20
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2018-03-07 11:42:28 -0800

    Ssdsds

---
+++ Game/Game.java
@@ -0,0 +1,30 @@
+/* Game.java by Mr. Considine
+This class holds all of the data and methods
+to play a game of ConnectFour. */
+//import java.awt.*;
+
+public class Game {
+
+  public static void main(String[] args) {
+
+
+    // main method here
+
+
+    //create a ConnectFour board
+
+
+    // prompt player1 to place a piece
+
+
+    //Display board
+
+
+    // prompt player2 to place a piece
+
+
+    // Display board and repeat
+  }
+
+  // method for displaying the board
+}
